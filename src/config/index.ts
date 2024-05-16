b651ac08ccd08b88396e73a430a234d9018e37d0
xiaoqiji <1150396540@qq.com>
2024-05-16 11:34:21 +0800

    path

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,6 +15,6 @@ const BASE_URL_MAP = {
 export const IS_SUB_DOMAIN = ['GITEE_PAGES', 'GP_PAGES'].includes(import.meta.env.VITE_ENV)
 export const IS_GITEE_PAGES = ['GITEE_PAGES'].includes(import.meta.env.VITE_ENV)
 export const BASE_URL = BASE_URL_MAP[import.meta.env.VITE_ENV]
-export const IMG_URL = BASE_URL + '/images/'
-export const FILE_URL = BASE_URL + '/data/'
+export const IMG_URL = BASE_URL + '/my-douyin-demo/images/'
+export const FILE_URL = BASE_URL + '/my-douyin-demo/data/'
 export const IS_DEV = process.env.NODE_ENV !== 'production'
